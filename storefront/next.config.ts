5975c01ea0d72bbbe2484c8285ddfded3386442f
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-03 20:06:02 -0500

    Refactor next.config.ts for image remote patterns

--- storefront/next.config.ts
+++ storefront/next.config.ts
@@ -1,59 +1,53 @@
-images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "mercur-connect.s3.eu-central-1.amazonaws.com",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "api.mercurjs.com",
-      pathname: "/**",
-    },
-    {
-      protocol: "http",
-      hostname: "localhost",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "api-sandbox.mercurjs.com",
-      pathname: "/static/**",
-    },
-    {
-      protocol: "https",
-      hostname: "i.imgur.com",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "s3.eu-central-1.amazonaws.com",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "blkmrktbucket.up.railway.app",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "freeblackmarket.up.railway.app",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "ibis.up.railway.app",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "*.proxy.rlwy.net",
-      pathname: "/**",
-    },
-  ],
-},
+import { defineConfig } from "next";
+
+export default defineConfig({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mercur-connect.s3.eu-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "api.mercurjs.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "api-sandbox.mercurjs.com",
+        pathname: "/static/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.eu-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "blkmrktbucket.up.railway.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "freeblackmarket.up.railway.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ibis.up.railway.app",
+        pathname: "/**",
+      },
+    ],
+  },
+});
