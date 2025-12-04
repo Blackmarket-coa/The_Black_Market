ba482f98671555188bef5dc86ca13b5fca9b29d0
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-03 20:00:18 -0500

    Add remote pattern for proxy.rlwy.net

--- storefront/next.config.ts
+++ storefront/next.config.ts
@@ -3,18 +3,22 @@ images: {
     {
       protocol: "https",
       hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      pathname: "/**",
     },
     {
       protocol: "https",
       hostname: "mercur-connect.s3.eu-central-1.amazonaws.com",
+      pathname: "/**",
     },
     {
       protocol: "https",
       hostname: "api.mercurjs.com",
+      pathname: "/**",
     },
     {
       protocol: "http",
       hostname: "localhost",
+      pathname: "/**",
     },
     {
       protocol: "https",
@@ -24,22 +28,32 @@ images: {
     {
       protocol: "https",
       hostname: "i.imgur.com",
+      pathname: "/**",
     },
     {
       protocol: "https",
       hostname: "s3.eu-central-1.amazonaws.com",
+      pathname: "/**",
     },
     {
       protocol: "https",
       hostname: "blkmrktbucket.up.railway.app",
+      pathname: "/**",
     },
     {
       protocol: "https",
       hostname: "freeblackmarket.up.railway.app",
+      pathname: "/**",
     },
     {
       protocol: "https",
       hostname: "ibis.up.railway.app",
+      pathname: "/**",
+    },
+    {
+      protocol: "https",
+      hostname: "*.proxy.rlwy.net",
+      pathname: "/**",
     },
   ],
 },
