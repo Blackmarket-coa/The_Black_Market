fade4c8f6c49a59eb1dd54fe1db81c0c8eb67cf7
Black Market Crash Dummy <foodcoa@gmail.com>
2025-12-03 20:17:01 -0500

    Update image remote patterns in next.config.ts

--- storefront/next.config.ts
+++ storefront/next.config.ts
@@ -11,7 +11,61 @@ export default defineConfig({
       {
         protocol: "https",
         hostname: "mercur-connect.s3.eu-central-1.amazonaws.com",
+      },// next.config.ts
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "mercur-connect.s3.eu-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "api.mercurjs.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "api-sandbox.mercurjs.com",
+        pathname: "/static/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.eu-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "blkmrktbucket.up.railway.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "freeblackmarket.up.railway.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ibis.up.railway.app",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
       {
         protocol: "https",
         hostname: "api.mercurjs.com",
